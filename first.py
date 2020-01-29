ca0deec49116ac45baa07629dc7938c950a595ff
Paul Silevitch <paul@silevitch.com>
2020-01-28 21:56:16 -0500

    adding first two scripts

---
+++ first.py
@@ -0,0 +1,31 @@
+from turtle import *
+import time
+import math
+
+shape('turtle')
+
+speed(1)
+
+forward(100)
+
+right(90)
+
+forward(100)
+
+right(90)
+
+forward(100)
+
+right(90)
+
+forward(100)
+
+right(45)
+
+forward (math.sqrt(5000))
+
+right(90)
+
+forward(math.sqrt(5000))
+
+time.sleep(5)
